9549b125bfb5babf4ebef89fc916c7ee6a422153
alumnos alumnos <alumnos@new.edu.ar>
2024-06-10 15:54:02 -0300

    Mi primer commit

---
+++ Actividad1.py
@@ -0,0 +1,30 @@
+##EJERCICIOS CONDICIONALES
+##Actividad 1 
+
+a=int(input("Dame tu edad"))
+if a<18:
+    print("sos menor de edad")
+else:
+    if a>18:
+        print("sos mayor")
+        
+##Actividad 2
+a=int(input("insertar contraseña"))
+if a ="contraseña"
+     print("contraseña correcta")
+else:
+    if a!=("contraseña"):
+        print("contraseña incorrecta")
+    
+##Actividad 3 
+a=int(input("dame un numero:"))
+b=int(input"dame otro numero:")
+while a =0:
+    print("a/b")
+    else("ERROR")
+##Actividad 4
+a=int(input("dame un valor a analizar"))
+if a%2 == 0:
+    print("Es par")
+else:
+    print("Es impar")
---
+++ ActividadN.py
@@ -0,0 +1,14 @@
+##Actividad
+#Crear un codigo de python que pregunte el valor de la temperatura y en caso que:
+#- si temperatura >10 declarar azul
+#- Si temperatura <20: nivel verde 
+# si temperatura <30 nivel rojo
+a=int(input("cual es la temperatura de hoy"))
+if a>10:
+     print(nivel azul)
+else:
+    if a <20:
+        print("nivel verde")
+        elif:
+            if a<30
+        print("nivel rojo")
